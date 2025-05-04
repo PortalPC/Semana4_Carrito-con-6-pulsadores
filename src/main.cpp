9df2c8bdba5d90e4085f9cb1049911e10fe97965
PortalPC <ian.portal@usil.pe>
2025-05-03 20:42:37 -0500

    Actualización_Portal_1

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,23 @@
-#include <Arduino.h>
+#define F_cpu 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int main(void){
+  DDRB |=0x0F;
+  DDRD &= ~0xFC
+  PORTD |= 0xFC
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+  while(1){
+    if(!(PIND & (1<<PD2))){
+      PORTB = 0b00000101;
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+    }else if(!(PIND & (1<<PD3))){
+      PORTB = 0b00001010;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+    }else if(!(PIND & (1<<PD4))){
+      PORTB = 0b00001001;
+
+    }else if(!(PIND & (1<<PD5))){
+      PORTB = 0b00000110;
+
+    }else if 
